db179b24c1f76799f79edca8683b3ad2cfbb1c65
ihor <ihor.bielchenko@gmail.com>
2023-05-22 10:34:59 +0200

    Update database.

--- src/api/neuron/neuron-http-tcp.controller.ts
+++ src/api/neuron/neuron-http-tcp.controller.ts
@@ -1,8 +1,14 @@
 import { 
 	Controller,
 	Get,
-	Param,
+	Query,
+	MethodNotAllowedException,
 } from '@nestjs/common';
+import { strToArr as utilsFormatStrToArr } from '@nest-datum-utils/format';
+import { 
+	str as utilsCheckStr,
+	numericInt as utilsCheckNumericInt, 
+} from '@nest-datum-utils/check';
 import { TransportService } from '@nest-datum/transport';
 import { HttpTcpController } from '@nest-datum-common/controllers-v2.2.0';
 
@@ -15,5 +21,33 @@ export class NeuronHttpTcpController extends HttpTcpController {
 		protected transport: TransportService,
 	) {
 		super();
+	}
+
+	async validateStep(options: object = {}) {
+		if (!utilsCheckNumericInt(options['id'])) {
+			throw new MethodNotAllowedException(`Property "id" is not valid.`);
+		}
+		if (!utilsCheckStr(options['value'])) {
+			throw new MethodNotAllowedException(`Property "value" is not valid.`);
+		}		
+		return {
+			id: Number(options['id']),
+			value: String(options['value'] ?? ''),
+		};
+	}
+
+	@Get('step')
+	async step(
+		@Query('id') id: number,
+		@Query('value') value: string,
+	) {
+		return await this.serviceHandlerWrapper(
+			async () => await this.transport.send({
+			name: this.serviceName, 
+			cmd: `${this.entityName}.step`,
+		}, await this.validateStep({
+			id,
+			value,
+		})));
 	}
 }
