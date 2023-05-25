5ef09efaeb03ae9fe57dc67b1518f9c3e6ba7a07
ihor <ihor.bielchenko@gmail.com>
2023-05-25 21:05:35 +0200

    Test

--- src/api/neuron/neuron-tcp.controller.ts
+++ src/api/neuron/neuron-tcp.controller.ts
@@ -57,8 +57,8 @@ export class NeuronTcpController extends TcpController {
 		return await super.update(payload);
 	}
 
-	@EventPattern('neuron.create')
-	async create(payload: object = {}) {
+	@EventPattern('neuron.step')
+	async step(payload: object = {}) {
 		console.log('111111111111111', { ...payload });
 
 		const { id, value } = await this.validateStep(payload);
